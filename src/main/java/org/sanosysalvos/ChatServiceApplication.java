768c7549ef01121170e59a7b31e6ae85e60528f1
Rodrigo Baeza <94202438+Electro-Kerubin@users.noreply.github.com>
2026-06-29 20:55:46 -0400

    fix: llapooooooo

--- src/main/java/org/sanosysalvos/ChatServiceApplication.java
+++ src/main/java/org/sanosysalvos/ChatServiceApplication.java
@@ -2,8 +2,9 @@ package org.sanosysalvos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { UserDetailsServiceAutoConfiguration.class })
 public class ChatServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ChatServiceApplication.class, args);
